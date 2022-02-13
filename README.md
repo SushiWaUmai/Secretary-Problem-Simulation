e1751734cc8fdf21a80dcba03de479e6318a3dff
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-13 23:43:43 +0100

    feat: :tada: add project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Secretary Problem Simulation
+
+Simulation of the [Secretary Problem](https://en.wikipedia.org/wiki/Secretary_problem)
+
+## Requirements
+- python3
+- pip or pip3
+
+## Installation
+```bash
+# if you have pip3
+pip3 install -r requirements.txt
+
+# if you use pip
+pip install -r requirements.txt
+```
+
+## Usage
+```bash
+python3 main.py
+# or
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+# secretary problem
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+n_dates = 100
+max_score = 100
+samples = 100
+
+# generate a random numpy matrix of n x samples numbers between 0 and max_score
+x = np.random.randint(0, max_score, (n_dates, samples))
+y = np.zeros(x.shape)
+
+for s in tqdm(range(samples)):
+    for i in range(n_dates):
+        # take the max of the first i dates
+        nth_max = np.max(x[0:i+1, s])
+
+        # get the next best score
+        current = x[i, s]
+
+        # if the current score is not the maximum
+        if current != nth_max:
+            # date until you get a better score than the first i dates
+            # take the last one if you didn't find a better score
+            for j in range(i, n_dates):
+                current = x[j, s]
+                if x[j, s] > nth_max:
+                    break
+                
+        # set the result to the current score
+        y[i, s] = current
+
+y = np.average(y, axis=1)
+
+# plot the data
+plt.xlabel("Woman where I start to look for the highest one")
+plt.ylabel("Expected Score")
+plt.plot(y)
+
+# show the plot
+plt.show()
