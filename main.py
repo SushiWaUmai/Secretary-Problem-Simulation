352859f3fcef3fa83fa6b57c29e0e40415ea8cd5
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-15 18:34:12 +0100

    perf: :zap: remove if statement for performance

--- main.py
+++ main.py
@@ -5,39 +5,33 @@ from tqdm import tqdm
 
 def show_plot(x):
     # plot the data
-    plt.xlabel("Woman where I start to look for the highest one")
+    plt.xlabel("Heuristic Parameter")
     plt.ylabel("Expected Score")
     plt.plot(x)
 
     # # show the plot
     # plt.show()
 
-n_dates = 100
-max_score = 10000
-samples = 1000
+n = 100
+samples = 100
 
 def main():
     # generate a random numpy matrix of n x samples numbers between 0 and max_score
-    x = np.random.randint(0, max_score, (n_dates, samples))
+    x = np.random.uniform(size=(n, samples))
     y = np.zeros(x.shape)
 
     for s in tqdm(range(samples)):
-        for i in range(n_dates):
+        for i in range(n):
             # take the max of the first i dates
             nth_max = np.max(x[0:i+1, s])
 
-            # get the next best score
-            current = x[i, s]
+            # date until you get a better score than the first i dates
+            # take the last one if you didn't find a better score
+            for j in range(i, n):
+                current = x[j, s]
+                if current >= nth_max:
+                    break
 
-            # if the current score is not the maximum
-            if current != nth_max:
-                # date until you get a better score than the first i dates
-                # take the last one if you didn't find a better score
-                for j in range(i, n_dates):
-                    current = x[j, s]
-                    if x[j, s] > nth_max:
-                        break
-                    
             # set the result to the current score
             y[i, s] = current
 
